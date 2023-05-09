5b8006b2b748d1dc39a4dffebe86cba977ad6729
Ricardo <ric.dalvarez@hotmail.com>
2023-05-09 17:52:09 +0200

    Fix N3 E1

--- exercise.js
+++ exercise.js
@@ -31,39 +31,6 @@ riki.dirNom()
 //nivel3
 //exercise 1
 
-//FIRST APPROACH
-class Ordenador {
-	constructor() {
-		throw new Error("no se puede instanciar Ordenador")
-	}
-	test(params) {
-		console.log("this is a computer" + " " + params)
-	}
-}
-
-class Mac {
-	constructor(price, size) {
-		this.price = price;
-		this.size = size;
-	}
-	price() { console.log(this.price) }
-}
-
-let macbook = new Mac(100, 13);
-
-macbook.prototype = Object.create(Ordenador.prototype);
-
-console.log(macbook.prototype.test("test"));
-
-
-//SECOND APPROACH
-function createOrdenador(string) {
-	let computer = Object.create(Ordenador.prototype)
-	computer.name = string
-	return computer
-}
-
-//THIRD APPROACH
 function Employee() {
 	if (this.constructor === Employee) {
 		throw new Error("FYI: Instance of Abstract class cannot be instantiated");
@@ -81,11 +48,8 @@ function Manager(fullName) {
 Manager.prototype = Object.create(Employee.prototype);
 
 let test = new Manager("jamming")
-
-//test
 console.log(test.display())
-createOrdenador("mac")
-createOrdenador("asus")
+
 
 
 
